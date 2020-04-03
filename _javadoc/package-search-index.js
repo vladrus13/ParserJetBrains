4e068be7b84e8b9d241066905959317459e92a2b
vladrus13 <vladrus13rus@yandex.ru>
2020-04-04 02:22:22 +0300

    Add JavaDoc (finally)

---
+++ _javadoc/package-search-index.js
@@ -0,0 +1,4 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "url": "allpackages-index.html"
+}, {"l": "ru.vladrus13"}, {"l": "ru.vladrus13.exception"}, {"l": "ru.vladrus13.operation"}];
---
+++ _javadoc/type-search-index.js
@@ -0,0 +1,25 @@
+typeSearchIndex = [{"l": "All Classes", "url": "allclasses-index.html"}, {
+    "p": "ru.vladrus13.operation",
+    "l": "BinaryOperation"
+}, {"p": "ru.vladrus13.exception", "l": "BracketBalanceException"}, {
+    "p": "ru.vladrus13.operation",
+    "l": "CallOperation"
+}, {"p": "ru.vladrus13.operation", "l": "Const"}, {
+    "p": "ru.vladrus13",
+    "l": "Converter"
+}, {"p": "ru.vladrus13.operation", "l": "Expression"}, {
+    "p": "ru.vladrus13.exception",
+    "l": "MissingOperatorException"
+}, {"p": "ru.vladrus13.operation", "l": "Negate"}, {
+    "p": "ru.vladrus13.operation",
+    "l": "Operation"
+}, {"p": "ru.vladrus13", "l": "Parser"}, {"p": "ru.vladrus13", "l": "ParserTest"}, {
+    "p": "ru.vladrus13.exception",
+    "l": "ParsingException"
+}, {"p": "ru.vladrus13", "l": "Simplifier"}, {"p": "ru.vladrus13", "l": "Parser.Token"}, {
+    "p": "ru.vladrus13.operation",
+    "l": "TYPE"
+}, {"p": "ru.vladrus13.exception", "l": "UnknownOperationException"}, {
+    "p": "ru.vladrus13.operation",
+    "l": "Variable"
+}, {"p": "ru.vladrus13.exception", "l": "WrongParamException"}];
